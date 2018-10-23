6bd9178ba721008bab0d3200d7acf26e8e03f091
ShreehariVaasishta <shreehari9481@gmail.com>
2018-10-23 20:04:10 +0530

    Revert "Power"
    This reverts commit 40a35b4963ecd00316500879dbf4352dd5a6c821.

--- device.mk
+++ device.mk
@@ -277,10 +277,7 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Power
-PRODUCT_PACKAGES += \
-   android.hardware.power@1.1-service-qti
-
+# Powerhint configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
