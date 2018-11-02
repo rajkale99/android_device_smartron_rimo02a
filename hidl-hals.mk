cb37ce3d749a3747919aeead663f25fc755e67d0
hari <shreehari9481@gmail.com>
2018-11-02 16:53:37 +0530

    Revert "s2: Switch to common basic USB HAL"
    This reverts commit eb978740c4b15b9177cddfd978acf067eb2f765c.

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.msm8976
 
 # Vibrator
 PRODUCT_PACKAGES += \
